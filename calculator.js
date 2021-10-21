376aff6593b0bc7306b8b35b3f53beca88da06d1
HartMason <hartmason85@gmail.com>
2021-10-20 19:42:07 -0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,5 @@
+// let num1 = 8;
+// let num2 = 4;
+
+let message = `The Brown fox`;
+console.log(message)
---
+++ index.js
@@ -0,0 +1,39 @@
+// console.log('Hello world')
+
+let num1 = 0;
+let num2 = 0;
+const num3 = 0;
+
+//these values are stored forever in our application//
+// console.log('num1', num1);
+
+//const num3 = 0 ... you can't reassaign a value to the const variable//
+num2 = 44; 
+
+
+console.log("num1", num1)
+console.log("num2", num2)
+
+// Data types:
+
+// Number // 1,2,3,4
+// Strings // 'Number one'   anything in quotes "1" 'one' 
+// Boolean // true, or false 
+// Undefined // example ... let num1; 
+// Null // no value yet but will have one so we write null 
+
+let myLuckyNumber = 42; 
+console.log(typeof myLuckyNumber)
+
+let myFavoriteString = 'Bass';
+console.log(typeof myFavoriteString)
+
+let myBool = false;
+console.log(typeof myBool)
+
+let myUndefined = undefined; 
+console.log(typeof myUndefined)
+
+let myNull = null;
+console.log(typeof myNull)
+//Null comes back as object 
