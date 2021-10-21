5fd15571092e988bdbc1c2b145e92678da902f80
HartMason <hartmason85@gmail.com>
2021-10-20 20:09:07 -0500

    calculator

--- calculator.js
+++ calculator.js
@@ -3,3 +3,36 @@
 
 let message = `The Brown fox`;
 console.log(message)
+
+let age = 18;
+
+if(age > 21) {
+    console.log('welcome to the club')
+
+    console.log('better luck next time')
+}   else if (age < 60) {
+    console.log('senior discount')
+}
+
+// =
+const name = "Matt"
+let countOfApples = 4;
+let countOfOranges = "2"
+
+let puppyIsCute = false;
+
+if(puppyIsCute !== true) {
+    console.log('that is an ugly dog');
+}
+
+//==
+// ===
+
+'Matt' == 'Matt'
+true
+'Matt' === 'Bonnie'
+false
+//=== checking value and type
+//== only checks value not type
+
+//use triple === all the time!!!
